c8a7aeb462695f24335ec636f735261959f21001
Jonathan Maverick <114144117+JonathanMaverick@users.noreply.github.com>
2025-12-01 13:15:16 +0700

    0124-BINARY-TREE-MAXIMUM-PATH-SUM Stats: Time: 21 ms (23.79%), Space: 23 MB (79.61%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -9,18 +9,18 @@ class Solution:
         res = root.val
         def dfs(node):
             nonlocal res
-            if not node:
-                return 0
-            
+            if not node: return 0
             leftMax = dfs(node.left)
             rightMax = dfs(node.right)
-            leftMax = max(leftMax, 0)
-            rightMax = max(rightMax, 0)
-
+            leftMax = max(0, leftMax)
+            rightMax = max(0, rightMax)
             res = max(res, node.val + leftMax + rightMax)
-            return node.val + max(leftMax, rightMax) 
-        
+            return node.val + max(leftMax, rightMax)
         dfs(root)
         return res
 
+        
+
+
+            
             
