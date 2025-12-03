6cf77e43c67e2b736050cf7b96ff647d316a284a
Jonathan Maverick <114144117+JonathanMaverick@users.noreply.github.com>
2025-12-03 12:27:42 +0700

    0079-WORD-SEARCH Stats: Time: 5033 ms (26.05%), Space: 18 MB (39.69%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,26 +1,30 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         ROWS, COLS = len(board), len(board[0])
-        stack = []
+        path = []
 
-        def dfs(r, c, i):
+        def dfs(i, r, c): 
             if i == len(word):
                 return True
-            
-            if min(r, c) < 0 or r >= ROWS or c >= COLS or word[i] != board[r][c]:
+            if r >= ROWS or c >= COLS or min(r,c) < 0 or word[i] != board[r][c]:
                 return False
             
             temp = board[r][c]
-            board[r][c] = "#"
-            stack.append((r,c))
-            res = (dfs(r + 1, c, i + 1) or dfs(r - 1, c, i + 1) or dfs(r, c + 1, i + 1) or dfs(r, c - 1, i + 1))
+            path.append([r, c])
+            board[r][c] = '#'
+            res = (
+                dfs(i + 1, r + 1, c) or
+                dfs(i + 1, r - 1, c) or
+                dfs(i + 1, r, c + 1) or
+                dfs(i + 1, r, c - 1)
+            )
             board[r][c] = temp
-            stack.remove((r,c))
+            path.pop()
             return res
-
-        for r in range(ROWS):
+            
+        for i in range(ROWS):
             for j in range(COLS):
-                if dfs(r, j, 0):
+                if dfs(0, i, j):
                     return True
         return False
             
