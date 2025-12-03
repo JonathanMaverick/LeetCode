8f0bacd53ca619593547c35bf9017c2a612adbb8
Jonathan Maverick <114144117+JonathanMaverick@users.noreply.github.com>
2025-12-03 13:57:06 +0700

    0212-WORD-SEARCH-II Stats: Time: 6439 ms (32.24%), Space: 19.2 MB (39.66%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -1,47 +1,42 @@
 class TrieNode:
     def __init__(self):
-        self.word = False
         self.child = {}
+        self.word = False
         
-    def addWords(self, word):
+    def addWord(self, word):
         curr = self
         for c in word:
             if c not in curr.child:
                 curr.child[c] = TrieNode()
             curr = curr.child[c]
-        curr.word = True 
-            
+        curr.word = True
+        
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        ROWS = len(board)
-        COLS = len(board[0])
         root = TrieNode()
-        
-        path = set()
-        res = set()
         for w in words:
-            root.addWords(w)
+            root.addWord(w)
 
-        def dfs(r, c, curr, word):
-            if r >= ROWS or c >= COLS or r < 0 or c < 0 or (r,c) in path or board[r][c] not in curr.child:
-                return 
-                
+        ROWS, COLS = len(board), len(board[0])
+        res, path = set(), set()
+
+        def dfs(r, c, word, curr):
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or (r,c) in path or board[r][c] not in curr.child:
+                return
+            
             path.add((r,c))
             curr = curr.child[board[r][c]]
             word += board[r][c]
             if curr.word:
                 res.add(word)
-
-            dfs(r + 1, c, curr, word)
-            dfs(r - 1, c, curr, word)
-            dfs(r, c + 1, curr, word)
-            dfs(r, c - 1, curr, word)
-            path.remove((r, c))
-
+            
+            dfs(r + 1, c, word, curr)
+            dfs(r - 1, c, word, curr)
+            dfs(r, c + 1, word, curr)
+            dfs(r, c - 1, word, curr)
+            path.remove((r,c))
+        
         for r in range(ROWS):
             for c in range(COLS):
-                dfs(r, c, root, "")
+                dfs(r, c, "", root)
         return list(res)
-                
-                         
-        
