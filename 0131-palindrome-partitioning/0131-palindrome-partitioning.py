9453a15cf661499884c409d719a335b447938fc7
Jonathan Maverick <114144117+JonathanMaverick@users.noreply.github.com>
2025-12-08 12:58:19 +0700

    0131-PALINDROME-PARTITIONING Stats: Time: 47 ms (66.8%), Space: 34.6 MB (44.59%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -2,28 +2,26 @@ class Solution:
     def partition(self, s: str) -> List[List[str]]:
         res = []
         part = []
-        def dfs(i, j):
-            if j >= len(s):
-                if i == j:
-                    res.append(part.copy())
+
+        def dfs(i):
+            if i >= len(s):
+                res.append(part.copy())
                 return
-            
-            if self.isPali(i, j, s):
-                part.append(s[i:j + 1])
-                dfs(j + 1, j + 1)
-                part.pop()
-            
-            dfs(i, j + 1)
-        dfs(0, 0)
+
+            for j in range(i, len(s)):
+                if self.isPali(i, j, s):
+                    part.append(s[i: j + 1])
+                    dfs(j + 1)
+                    part.pop()
+        dfs(0)
         return res
-            
-    
+                    
     def isPali(self, l, r, s):
         while l < r:
-            if not s[l] == s[r]:
+            if s[l] != s[r]:
                 return False
-            l += 1
-            r -= 1
+            l+=1
+            r-=1
         return True
+            
                 
-        
